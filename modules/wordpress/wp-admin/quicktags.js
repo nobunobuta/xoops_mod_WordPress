8632076ffca067b40d0ccc43b7b6dbe13a6aad34
nobunobuta <nobunobuta@nobunobuta.com>
2004-05-18 13:48:42 +0000

    textareaの名前変更に伴うfaceマーク挿入不具合対応

--- modules/wordpress/wp-admin/quicktags.js
+++ modules/wordpress/wp-admin/quicktags.js
@@ -370,17 +370,17 @@ theSelection = false;
 
 if (document.selection) 
 { 
-formObj.content.focus(); 
+formObj.wp_content.focus(); 
 theSelection = document.selection.createRange().text; // Get text selection 
 // Add tags around selection 
 document.selection.createRange().text = strIns + theSelection + strInsClose; 
-formObj.content.focus(); 
+formObj.wp_content.focus(); 
 theSelection = false; 
 return; 
 } 
 
-formObj.content.value += strIns + strInsClose; 
-formObj.content.focus(); 
+formObj.wp_content.value += strIns + strInsClose; 
+formObj.wp_content.focus(); 
 return; 
 
 }
