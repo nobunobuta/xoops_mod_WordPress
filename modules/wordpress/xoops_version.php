f45c2a34654270416c96cf3047c4f254ffc174b1
nobunobuta <nobunobuta@nobunobuta.com>
2004-05-19 14:04:36 +0000

    0.0.2cリリース

--- modules/wordpress/xoops_version.php
+++ modules/wordpress/xoops_version.php
@@ -3,7 +3,7 @@ $modversion['name'] = _MI_WORDPRESS_NAME;
 $modversion['dirname'] = 'wordpress';
 
 $modversion['description'] = _MI_WORDPRESS_DESC;
-$modversion['version'] = "0.02b";
+$modversion['version'] = "0.02c";
 $modversion['credits'] = "";
 $modversion['author'] = _MI_WORDPRESS_AUTHOR;
 $modversion['help'] = "help.html";
