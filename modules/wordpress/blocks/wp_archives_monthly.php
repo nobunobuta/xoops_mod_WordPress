223d7350670481b2c2ee3a69f5129fe0e7320d31
nobunobuta <nobunobuta@nobunobuta.com>
2004-05-27 15:01:01 +0000

    ブロックの表示オプションを幾つか追加

--- modules/wordpress/blocks/wp_archives_monthly.php
+++ modules/wordpress/blocks/wp_archives_monthly.php
@@ -1,6 +1,35 @@
 <?php
-function b_wp_archives_monthly_show($option)
+function b_wp_archives_monthly_edit($options)
 {
+	$form = "";
+	$form .= "Month List Style:&nbsp;";
+	if ( $options[0] == 0 ) {
+		$chk = " checked='checked'";
+	}
+	$form .= "<input type='radio' name='options[]' value='0'".$chk." />&nbsp;Simple List";
+	$chk = "";
+	if ( $options[0] == 1 ) {
+		$chk = " checked=\"checked\"";
+	}
+	$form .= "&nbsp;<input type='radio' name='options[]' value='1'".$chk." />&nbsp;Dropdown List";
+	$form .= "<br />Listing with count:&nbsp;";
+	if ( $options[1] == 1 ) {
+		$chk = " checked='checked'";
+	}
+	$form .= "<input type='radio' name='options[1]' value='1'".$chk." />&nbsp;"._YES."";
+	$chk = "";
+	if ( $options[1] == 0 ) {
+		$chk = " checked=\"checked\"";
+	}
+	$form .= "&nbsp;<input type='radio' name='options[1]' value='0'".$chk." />"._NO."";
+	return $form;
+}
+
+function b_wp_archives_monthly_show($options)
+{
+	$block_style =  ($options[0])?$options[0]:0;
+	$with_count =  ($options[1]==0)?false:true;
+
 	$id=1;
 	global $dateformat, $time_difference, $siteurl, $blogfilename;
 	global $tablelinks,$tablelinkcategories;
@@ -10,7 +39,18 @@ function b_wp_archives_monthly_show($option)
 	global $wp_bbcode, $use_bbcode, $wp_gmcode, $use_gmcode, $use_htmltrans, $wp_htmltrans, $wp_htmltranswinuni;
 	require_once(dirname(__FILE__).'/../wp-blog-header.php');
 	ob_start();
-	get_archives('monthly');
+	if ($block_style == 0) {
+	// Simple Listing
+		get_archives('monthly','','html', '','',$with_count);
+	} else {
+	// Dropdown Listing
+		echo '<form name="archiveform" action="">';
+		echo '<select name="archive_chrono" onchange="window.location = (document.forms.archiveform.archive_chrono[document.forms.archiveform.archive_chrono.selectedIndex].value);"> ';
+		echo '<option value="">'._WP_BY_MONTHLY.'</option>';
+		get_archives('monthly','','option', '','',$with_count);
+		echo '</select>';
+		echo '</form>';
+	}
 	$block['content'] = ob_get_contents();
 	ob_end_clean();
 	return $block;
--- modules/wordpress/blocks/wp_categories.php
+++ modules/wordpress/blocks/wp_categories.php
@@ -1,6 +1,35 @@
 <?php
-function b_wp_categories_show($option)
+function b_wp_categories_edit($options)
 {
+	$form = "";
+	$form .= "Category List Style:&nbsp;";
+	if ( $options[0] == 0 ) {
+		$chk = " checked='checked'";
+	}
+	$form .= "<input type='radio' name='options[]' value='0'".$chk." />&nbsp;Simple List";
+	$chk = "";
+	if ( $options[0] == 1 ) {
+		$chk = " checked=\"checked\"";
+	}
+	$form .= "&nbsp;<input type='radio' name='options[]' value='1'".$chk." />&nbsp;Dropdown List";
+	$form .= "<br />Listing with count:&nbsp;";
+	if ( $options[1] == 1 ) {
+		$chk = " checked='checked'";
+	}
+	$form .= "<input type='radio' name='options[1]' value='1'".$chk." />&nbsp;"._YES."";
+	$chk = "";
+	if ( $options[1] == 0 ) {
+		$chk = " checked=\"checked\"";
+	}
+	$form .= "&nbsp;<input type='radio' name='options[1]' value='0'".$chk." />"._NO."";
+	return $form;
+}
+
+function b_wp_categories_show($options)
+{
+	$block_style =  ($options[0])?$options[0]:0;
+	$with_count =  ($options[1])?$options[1]:0;
+
 	$id=1;
 	global $dateformat, $time_difference, $siteurl, $blogfilename;
 	global $tablelinks,$tablelinkcategories;
@@ -9,10 +38,25 @@ function b_wp_categories_show($option)
 	global $smilies_directory, $use_smilies, $wp_smiliessearch, $wp_smiliesreplace;
 	global $wp_bbcode, $use_bbcode, $wp_gmcode, $use_gmcode, $use_htmltrans, $wp_htmltrans, $wp_htmltranswinuni;
 	require_once(dirname(__FILE__).'/../wp-blog-header.php');
-	ob_start();
-	list_cats(0, 'All', 'name');
-	$block['content'] = ob_get_contents();
-	ob_end_clean();
+	if ($block_style == 0) {
+		// Simple Listing
+		ob_start();
+		list_cats(0, 'All', 'name','asc','',true,0,$with_count);
+		$block['content'] = ob_get_contents();
+		ob_end_clean();
+	} else {
+		// Dropdown Listing
+		$file = "$siteurl/$blogfilename";
+		$link = $file.$querystring_start.'cat'.$querystring_equal;
+		ob_start();
+		echo '<form name="listcatform" action="">';
+		$select_str = '<select name="cat" onchange="window.location = (\''.$link.'\'+document.forms.listcatform.cat[document.forms.listcatform.cat.selectedIndex].value);"> ';
+		dropdown_cats(1,_WP_LIST_CAT_ALL,'name','asc',0,$with_count);
+		echo '</form>';
+		$block_str = ob_get_contents();
+		ob_end_clean();
+		$block['content'] = ereg_replace('\<select name\=[^\>]*\>',$select_str,$block_str);
+	}
 	return $block;
 }
 ?>
