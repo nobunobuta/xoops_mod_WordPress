ac5f716b07475b23c98097c5d3ddde6ef16bd74d
nobunobuta <nobunobuta@nobunobuta.com>
2004-06-20 13:17:13 +0000

    モジュールの複製可能化 （Enable to clone this module) WordPressテンプレートやスタイルのXOOPSテーマとの連動を可能に(Can custmize WordPress templates and Style sheet with each Xoops themes)

--- modules/wordpress/header.php
+++ modules/wordpress/header.php
@@ -1,28 +1,32 @@
 <?php
 $xoopsOption['show_rblock'] =1;
+include_once (dirname(__FILE__)."/../../mainfile.php");
 include(XOOPS_ROOT_PATH.'/header.php');
+require('wp-blog-header.php');
 ob_start();
 ?>
 	<meta name="generator" content="WordPress <?php echo $wp_version; ?>" />
 	<!-- leave this for stats -->
-
-	<style type="text/css" media="screen">
-		@import url( <?php echo $siteurl; ?>/wp-layout.css );
-	</style>
-	
-	<link rel="stylesheet" type="text/css" media="print" href="<?php echo $siteurl; ?>/print.css" />
+<?php
+if (file_exists(XOOPS_ROOT_PATH.'/modules/wordpress'. (($wp_id=='-')?'':$wp_id) .'/themes/'.$xoopsConfig['theme_set'].'/wp-layout.css')) {
+	$themes = $xoopsConfig['theme_set'];
+} else {
+	$themes = "default";
+}
+?>
+	<link rel="stylesheet" type="text/css" media="screen" href="<?php echo $siteurl; ?>/themes/<?php echo $themes; ?>/wp-layout.css" />
+	<link rel="stylesheet" type="text/css" media="print" href="<?php echo $siteurl; ?>/themes/<?php echo $themes; ?>/print.css" />
 	<link rel="alternate" type="application/rdf+xml" title="RDF" href="<?php bloginfo('rdf_url'); ?>" />
 	<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
 	<link rel="alternate" type="text/xml" title="RSS .92" href="<?php bloginfo('rss_url'); ?>" />
 	<link rel="alternate" type="application/atom+xml" title="Atom 0.3" href="<?php bloginfo('atom_url'); ?>" />
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-    <link rel="EditURI" type="application/rsd+xml" title="RSD" href="<?php echo $siteurl; ?>/wp-rsd.php" />
-    <?php get_archives('monthly', '', 'link'); ?>
-	<?php // comments_popup_script(); // off by default ?>
+	<link rel="EditURI" type="application/rsd+xml" title="RSD" href="<?php echo $siteurl; ?>/wp-rsd.php" />
+<?php get_archives('monthly', '', 'link'); ?>
+<?php // comments_popup_script(); // off by default ?>
 <?php
 $my_header = ob_get_contents();
 ob_end_clean();
-//include(XOOPS_ROOT_PATH.'/header.php');
 ob_start();
 echo  bloginfo('name');
 $blog_name =  ob_get_contents();
