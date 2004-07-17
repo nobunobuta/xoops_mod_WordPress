25b49fa7061d737bd8cb8a22c69aa230a782c581
nobunobuta <nobunobuta@nobunobuta.com>
2004-07-17 03:38:35 +0000

    WordPress1.2のサンプルプラグインを追加

---
+++ modules/wordpress/wp-content/plugins/google-hilite.php
@@ -0,0 +1,143 @@
+<?php
+/*
+Plugin Name: Search Hilite
+Plugin URI: http://wordpress.org/#
+Description:_LANG_PG_GOOGLE_HILITE
+Version: 1.2
+Author: Ryan Boren
+Author URI: http://rboren.nu
+*/ 
+
+/* Highlighting code c/o Ryan Boren */
+function get_search_query_terms($engine = 'google') {
+    global $s, $s_array, $blog_charset;
+	$referer = urldecode($_SERVER[HTTP_REFERER]);
+	$query_array = array();
+	switch ($engine) {
+	case 'google':
+		// Google query parsing code adapted from Dean Allen's
+		// Google Hilite 0.3. http://textism.com
+		$query_terms = preg_replace('/^.*q=([^&]+)&?.*$/i','$1', $referer);
+		$query_terms = preg_replace('/\'|"/', '', $query_terms);
+		if (function_exists('mb_convert_encoding')) {
+			$query_terms = mb_convert_encoding($query_terms, $blog_charset, "auto");
+		}
+		$query_array = preg_split ("/[\s,\+\.]+/", $query_terms);
+		break;
+
+	case 'lycos':
+		$query_terms = preg_replace('/^.*query=([^&]+)&?.*$/i','$1', $referer);
+		$query_terms = preg_replace('/\'|"/', '', $query_terms);
+		if (function_exists('mb_convert_encoding')) {
+			$query_terms = mb_convert_encoding($query_terms, $blog_charset, "auto");
+		}
+		$query_array = preg_split ("/[\s,\+\.]+/", $query_terms);
+		break;
+
+	case 'yahoo':
+		$query_terms = preg_replace('/^.*p=([^&]+)&?.*$/i','$1', $referer);
+		$query_terms = preg_replace('/\'|"/', '', $query_terms);
+		if (function_exists('mb_convert_encoding')) {
+			$query_terms = mb_convert_encoding($query_terms, $blog_charset, "auto");
+		}
+		$query_array = preg_split ("/[\s,\+\.]+/", $query_terms);
+		break;
+
+    case 'wordpress':
+        // Check the search form vars if the search terms
+        // aren't in the referer.
+        if ( ! preg_match('/^.*s=/i', $referer)) {
+            if (isset($s_array)) {
+                $query_array = $s_array;
+            } else if (isset($s)) {
+                $query_array = array($s);
+            }
+
+            break;
+        }
+
+		$query_terms = preg_replace('/^.*s=([^&]+)&?.*$/i','$1', $referer);
+		$query_terms = preg_replace('/\'|"/', '', $query_terms);
+		$query_array = preg_split ("/[\s,\+\.]+/", $query_terms);
+        break;
+	}
+
+	return $query_array;
+}
+
+function is_referer_search_engine($engine = 'google') {
+	global $siteurl;
+	$referer = urldecode($_SERVER['HTTP_REFERER']);
+    //echo "referer is: $referer<br />";
+	if ( ! $engine ) {
+		return 0;
+	}
+
+	switch ($engine) {
+	case 'google':
+		if (preg_match('|^http://(www)?\.?google.*|i', $referer)) {
+			return 1;
+		}
+		break;
+
+    case 'lycos':
+		if (preg_match('|^http://search\.lycos.*|i', $referer)) {
+			return 1;
+		}
+        break;
+
+    case 'yahoo':
+		if (preg_match('|^http://search\.yahoo.*|i', $referer)) {
+			return 1;
+		}
+        break;
+
+    case 'wordpress':
+        if (preg_match("#^$siteurl#i", $referer)) {
+            return 1;
+        }
+        break;
+	}
+
+	return 0;
+}
+
+function hilite($text) {
+	$search_engines = array('wordpress', 'google', 'lycos', 'yahoo');
+
+	foreach ($search_engines as $engine) {
+		if ( is_referer_search_engine($engine)) {
+			$query_terms = get_search_query_terms($engine);
+			foreach ($query_terms as $term) {
+				if (!empty($term) && $term != ' ') {
+					if (!preg_match('/<.+>/',$text)) {
+						$text = preg_replace('/('.$term.')/i','<span class="hilite">$1</span>',$text);
+					} else {
+						$text = preg_replace('/(?<=>)([^<]+)?('.$term.')/i','$1<span class="hilite">$2</span>',$text);
+					}
+				}
+			}
+			break;
+		}
+	}
+
+	return $text;
+}
+
+function hilite_head() {
+	echo "
+<style type='text/css'>
+.hilite {
+	color: #000;
+	background-color: #ff8;
+}
+</style>
+";
+}
+
+// Highlight text and comments:
+add_filter('the_content', 'hilite');
+add_filter('comment_text', 'hilite');
+add_action('wp_head', 'hilite_head');
+
+?>
---
+++ modules/wordpress/wp-content/plugins/hello.php
@@ -0,0 +1,72 @@
+<?php
+/*
+Plugin Name: Hello Dolly
+Plugin URI: http://wordpress.org/#
+Description:_LANG_PG_HELLO_DOLLY
+Author: Matt Mullenweg
+Author URI: http://photomatt.net/
+*/ 
+
+// These are the lyrics to Hello Dolly
+$lyrics = "Hello, Dolly
+Well, hello, Dolly
+It's so nice to have you back where you belong
+You're lookin' swell, Dolly
+I can tell, Dolly
+You're still glowin', you're still crowin'
+You're still goin' strong
+We feel the room swayin'
+While the band's playin'
+One of your old favourite songs from way back when
+So, take her wrap, fellas
+Find her an empty lap, fellas
+Dolly'll never go away again
+Hello, Dolly
+Well, hello, Dolly
+It's so nice to have you back where you belong
+You're lookin' swell, Dolly
+I can tell, Dolly
+You're still glowin', you're still crowin'
+You're still goin' strong
+We feel the room swayin'
+While the band's playin'
+One of your old favourite songs from way back when
+Golly, gee, fellas
+Find her a vacant knee, fellas
+Dolly'll never go away
+Dolly'll never go away
+Dolly'll never go away again";
+
+// Here we split it into lines
+$lyrics = explode("\n", $lyrics);
+// And then randomly choose a line
+global $chosen;
+$chosen = wptexturize( $lyrics[ mt_rand(0, count($lyrics) ) ] );
+// This just echoes the chosen line, we'll position it later
+function hello_dolly() {
+	global $chosen;
+	echo "<p id='dolly'>$chosen</p>";
+}
+
+// Now we set that function up to execute when the admin_footer action is called
+add_action('admin_footer', 'hello_dolly');
+
+// We need some CSS to position the paragraph
+function dolly_css() {
+	echo "
+	<style type='text/css'>
+	#dolly {
+		position: absolute;
+		top: 5px;
+margin: 0; padding: 0;
+		right: 3em;
+		font-size: 20px;
+		color: #666;
+	}
+	</style>
+	";
+}
+
+add_action('admin_head', 'dolly_css');
+
+?>
