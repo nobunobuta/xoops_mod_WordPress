213b288d4ea2fd3fd8b23dfc64e1becdad394297
nobunobuta <nobunobuta@nobunobuta.com>
2004-07-20 12:13:01 +0000

    ブロック関連の細かな不具合に対応

--- modules/wordpress/blocks/wp_authors.php
+++ modules/wordpress/blocks/wp_authors.php
@@ -5,12 +5,29 @@ if( ! defined( 'WP_AUTHORS_INCLUDED' ) ) {
 
 	function b_wp_authors_edit($options)
 	{
-		$form = "";
+		$form = "<table width='100%'>";
+
+		$form .= "<tr><td width='40%'>Listing with count:</td>";
+		$chk = ( $options[0] == 1 ) ? " checked='checked'" : "";
+		$form .= "<td><input type='radio' name='options[0]' value='1'".$chk." />&nbsp;"._YES."&nbsp;";
+		$chk = ( $options[0] == 0 ) ? " checked='checked'" : "";
+		$form .= "<input type='radio' name='options[0]' value='0'".$chk." />&nbsp;"._NO."</td></tr>";
+
+		$form .= "<tr><td>Show RSS2 feed icon:</td>";
+		$chk = ( $options[1] == 1 ) ? " checked='checked'" : "";
+		$form .= "<td><input type='radio' name='options[1]' value='1'".$chk." />&nbsp;"._YES."&nbsp;";
+		$chk = ( $options[1] == 0 ) ? " checked='checked'" : "";
+		$form .= "<input type='radio' name='options[1]' value='0'".$chk." />&nbsp;"._NO."</td></tr>";
+
+		$form .= "</table>";
 		return $form;
 	}
 
 	function b_wp_authors_show($options, $wp_num="")
 	{
+		$with_count =  (empty($options[0]))? 0 : $options[0];
+		$show_rss2_icon = (empty($options[1]))? 0 : $options[1];
+
 		global $wpdb, $siteurl,  $wp_id, $wp_inblock ,$user_cache;
 
 		$id=1;
@@ -22,12 +39,12 @@ if( ! defined( 'WP_AUTHORS_INCLUDED' ) ) {
 			require(dirname(__FILE__).'/../wp-config.php');
 			$wp_inblock = 0;
 		}
-		$optioncount = true;
+		$optioncount = ($with_count == 1);
 		$exclude_admin = false;
 		$show_fullname = false;
 		$hide_empty = true;
-		$feed = 'rss2';
-		$feed_image = $siteurl.'/wp-images/rss-mini.gif';
+		$feed = ($show_rss2_icon == 1) ? 'rss2' : '' ;
+		$feed_image = ($show_rss2_icon == 1) ? $siteurl.'/wp-images/rss-mini.gif' : '';
 		ob_start();
 		list_authors($optioncount,$exclude_admin,$show_fullname,$hide_empty,$feed,$feed_image);
 		$block['content'] = ob_get_contents();
