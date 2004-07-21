be0ba05032582b3936b8eee04fd9b1f16e62a4f5
nobunobuta <nobunobuta@nobunobuta.com>
2004-07-21 14:05:33 +0000

    管理メニューのメッセージ修正

--- modules/wordpress/admin/menu.php
+++ modules/wordpress/admin/menu.php
@@ -2,5 +2,5 @@
 $adminmenu[0]['title'] = _MI_WORDPRESS_AD_MENU1;
 $adminmenu[0]['link'] = "wp-admin/options.php";
 $adminmenu[1]['title'] = _MI_WORDPRESS_AD_MENU2;
-$adminmenu[2]['link'] = "admin/myblocksadmin.php";
+$adminmenu[1]['link'] = "admin/myblocksadmin.php";
 ?>
