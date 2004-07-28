911bdc2adc5ffa522134c37ec2f82cfc846afd72
nobunobuta <nobunobuta@nobunobuta.com>
2004-07-28 04:05:33 +0000

    WordPress1.2で使用しているキャッシュの初期化を追加

--- modules/wordpress/blocks/wp_contents.php
+++ modules/wordpress/blocks/wp_contents.php
@@ -14,7 +14,7 @@ if( ! defined( 'WP_CONTENTS_INCLUDED' ) ) {
 	function b_wp_contents_show($options,$wp_num="") {
 		$no_posts = (empty($options[0]))? 10 : $options[0];
 
-		global $wpdb, $siteurl, $post, $use_cache;
+		global $wpdb, $siteurl, $post, $use_cache, $category_cache, $comment_count_cache;
 		global $smilies_directory,  $wp_smiliessearch, $wp_smiliesreplace ,$authordata;
 		global $wp_bbcode,  $wp_gmcode,   $wp_htmltrans, $wp_htmltranswinuni;
 		global $wp_id, $wp_inblock, $xoopsConfig, $previousday, $time_difference ,$day;
@@ -28,7 +28,6 @@ if( ! defined( 'WP_CONTENTS_INCLUDED' ) ) {
 			require(dirname(__FILE__).'/../wp-config.php');
 			$wp_inblock = 0;
 		}
-
 		global $dateformat,$timeformat;
 		
 		$dateformat = stripslashes(get_settings('date_format'));
@@ -42,6 +41,33 @@ if( ! defined( 'WP_CONTENTS_INCLUDED' ) ) {
 		$request .= " GROUP BY {$wpdb->posts[$wp_id]}.ID ORDER BY post_date DESC LIMIT 0, $no_posts";
 		$lposts = $wpdb->get_results($request);
 		
+		if ($lposts) {
+		// Get the categories for all the posts
+			foreach ($lposts as $post) {
+				$post_id_list[] = $post->ID;
+			}
+			$post_id_list = implode(',', $post_id_list);
+
+			$dogs = $wpdb->get_results("SELECT DISTINCT
+				ID, category_id, cat_name, category_nicename, category_description, category_parent
+				FROM {$wpdb->categories[$wp_id]}, {$wpdb->post2cat[$wp_id]}, {$wpdb->posts[$wp_id]}
+				WHERE category_id = cat_ID AND post_id = ID AND post_id IN ($post_id_list)");
+
+		    foreach ($dogs as $catt) {
+				$category_cache[$wp_id][$catt->ID][] = $catt;
+			}
+
+		    // Do the same for comment numbers
+			$comment_counts = $wpdb->get_results("SELECT ID, COUNT( comment_ID ) AS ccount
+				FROM {$wpdb->posts[$wp_id]}
+				LEFT JOIN {$wpdb->comments[$wp_id]} ON ( comment_post_ID = ID  AND comment_approved =  '1')
+				WHERE post_status =  'publish' AND ID IN ($post_id_list)
+				GROUP BY ID");
+
+			foreach ($comment_counts as $comment_count) {
+				$comment_count_cache[$wp_id]["$comment_count->ID"] = $comment_count->ccount;
+			}
+		}
 		$blog = 1;
 		$block = array();
 		$block['use_theme_template'] = get_xoops_option('wordpress'.$wp_num,'use_theme_template');
@@ -68,7 +94,7 @@ if( ! defined( 'WP_CONTENTS_INCLUDED' ) ) {
 				$content['permlink'] = get_permalink();
 		//
 				ob_start();
-				the_author();
+				the_author_posts_link();
 				$content['author'] = ob_get_contents();
 				ob_end_clean();
 		//
@@ -117,10 +143,10 @@ if( ! defined( 'WP_CONTENTS_INCLUDED' ) ) {
 		}
 
 		function b_wp'.$i.'_contents_show($options) {
-			global $wpdb, $siteurl, $post, $use_cache;
+			global $wpdb, $siteurl, $post, $use_cache, $category_cache, $comment_count_cache;
 			global $smilies_directory,  $wp_smiliessearch, $wp_smiliesreplace ,$authordata;
 			global $wp_bbcode,  $wp_gmcode,   $wp_htmltrans, $wp_htmltranswinuni;
-			global $wp_id, $wp_inblock, $xoopsConfig, $previousday, $time_difference, $day ;
+			global $wp_id, $wp_inblock, $xoopsConfig, $previousday, $time_difference ,$day;
 			$wp_id = "'.$i.'";
 			$wp_inblock = 1;
 			require(XOOPS_ROOT_PATH."/modules/wordpress'.$i.'/wp-config.php");
