ad341080232e17cdf35301912ca429d62e9edfa6
nobunobuta <nobunobuta@nobunobuta.com>
2004-07-29 14:11:54 +0000

    複数ブログ設置時のテンプレート・スタイルシート取得先がおかしいバグに対応

--- modules/wordpress/index.php
+++ modules/wordpress/index.php
@@ -5,11 +5,11 @@ include("header.php");
 wp_head();
 // Uncomment the next line if you want to track blog updates from weblogs.com
 //include_once(ABSPATH.WPINC.'/links-update-xml.php');
-if (file_exists(ABSPATH.'/themes/'.$xoopsConfig['theme_set'].'/index-template.php')) {
+if (file_exists(XOOPS_ROOT_PATH.'/modules/wordpress'.(($wp_id=='-') ?'':$wp_id).'/themes/'.$xoopsConfig['theme_set'].'/index-template.php')) {
 	$themes = $xoopsConfig['theme_set'];
 } else {
 	$themes = "default";
 }
-include(ABSPATH.'/themes/'.$themes.'/index-template.php');
+include(XOOPS_ROOT_PATH.'/modules/wordpress'.(($wp_id=='-') ?'':$wp_id).'/themes/'.$themes.'/index-template.php');
 include(XOOPS_ROOT_PATH."/footer.php");
 ?>
