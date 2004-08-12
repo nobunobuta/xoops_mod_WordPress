93b59c60771d281ba911a0440666020c09c6ee62
nobunobuta <nobunobuta@nobunobuta.com>
2004-08-12 14:30:16 +0000

    携帯用Viewerへの自動リダイレクトに対応

--- modules/wordpress/index.php
+++ modules/wordpress/index.php
@@ -2,6 +2,9 @@
 /* Don't remove these lines. */
 $blog = 1;
 include("header.php");
+if (preg_match('/DoCoMo/', $_SERVER['HTTP_USER_AGENT']) and file_exists(dirname(__FILE__)."/wp-ktai.php")) { 
+	header('Location: ' . XOOPS_URL.'/modules/wordpress'.(($wp_id=='-') ?'':$wp_id)."/wp-ktai.php\n");
+}
 wp_head();
 // Uncomment the next line if you want to track blog updates from weblogs.com
 //include_once(ABSPATH.WPINC.'/links-update-xml.php');
