619d70d6938ee4894d971eb6d7e7b2ff7af2ae61
nobunobuta <nobunobuta@nobunobuta.com>
2004-09-05 03:33:43 +0000

    XOOPSモジュールで不要なファイルの整理

--- modules/wordpress/wp-login.php
+++
@@ -1,9 +0,0 @@
-<?php
-	include_once '../../mainfile.php';
-	if ($xoopsUser) {
-		$loc = XOOPS_URL."/modules/". basename(dirname(__FILE__));
-	} else {
-		$loc = XOOPS_URL."/user.php";
-	}
-	redirect_header($loc, 1, "This function is not available in XOOPS Environment.");
-?>
