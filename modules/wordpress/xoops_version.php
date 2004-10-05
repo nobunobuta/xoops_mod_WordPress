a6d4cd5b60ca7e3e4b860690aa52a2e19ea4de28
nobunobuta <nobunobuta@nobunobuta.com>
2004-10-05 13:53:30 +0000

    コメント一覧出力用のオプションを追加

--- modules/wordpress/xoops_version.php
+++ modules/wordpress/xoops_version.php
@@ -143,7 +143,7 @@ $modversion['blocks'][7]['name'] = sprintf( _MI_WORDPRESS_BNAME7 , $my_wp_dirnum
 $modversion['blocks'][7]['description'] = _MI_WORDPRESS_BDESC7;
 $modversion['blocks'][7]['show_func'] = "b_wp{$my_wp_dirnumber}_recent_comments_show";
 $modversion['blocks'][7]['edit_func'] = "b_wp{$my_wp_dirnumber}_recent_comments_edit";
-$modversion['blocks'][7]['options'] = "0|10|0";
+$modversion['blocks'][7]['options'] = "0|10|0|0|1";
 
 $modversion['blocks'][8]['file'] = "wp_contents.php";
 $modversion['blocks'][8]['name'] = sprintf( _MI_WORDPRESS_BNAME8 , $my_wp_dirnumber );
