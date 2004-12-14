f057e3dadcdfdc29d4298043495a2f019f6a2bee
nobunobuta <nobunobuta@nobunobuta.com>
2004-12-14 14:35:27 +0000

    コメントスパム対策用に確認画面を追加する事を可能に

--- modules/wordpress/wp-comments-post.php
+++ modules/wordpress/wp-comments-post.php
@@ -8,7 +8,6 @@ param('comment','html','');
 param('comment_post_ID','integer','');
 param('redirect_to','string','');
 param('action','string','');
-param('ticket','string','');
 
 $user_ip = $_SERVER['REMOTE_ADDR'];
 $user_domain = gethostbyaddr($user_ip);
@@ -16,6 +15,7 @@ $user_domain = gethostbyaddr($user_ip);
 if (!is_email($email)) {
 	$email = '';
 }
+
 $url_struct = parse_url($url);
 if (!$url_struct) {
 	$url = '';
@@ -32,10 +32,8 @@ if (isset($url_struct['scheme']) && !preg_match('/^http[s]?$/',$url_struct['sche
 }
 
 $original_comment = $comment;
-$user_ip = $_SERVER['REMOTE_ADDR'];
-$user_domain = gethostbyaddr($user_ip);
 
-	$commentstatus = $wpdb->get_var("SELECT comment_status FROM {$wpdb->posts[$wp_id]} WHERE ID = $comment_post_ID");
+$commentstatus = $wpdb->get_var("SELECT comment_status FROM {$wpdb->posts[$wp_id]} WHERE ID = $comment_post_ID");
 if ('closed' == $commentstatus) {
    	redirect_header($location ,5,_LANG_WPCP_SORRY_ITEM);
    	exit();
@@ -59,105 +57,55 @@ if ((get_settings('use_comment_preview'))&&($action!='confirm')) {
 	include('header.php');
 	$comment = balanceTags($comment, 1);
 	$comment = convert_chars($comment);
-	$comment = format_to_post($comment);
 	$comment = apply_filters('post_comment_text', $comment);
 
-	$comment_new = apply_filters('comment_text', $comment);
-	$author_new = apply_filters('comment_author', $comment_new);
-?>
-<div id="wpMainContent">
-<h2 id="comments"><?php echo _LANG_WPCM_COM_TITLE." (Preview)"; ?></h2>
-<ol id="commentlist">
-	<li id="comment-prev ?>">
-	<?php echo stripslashes($comment_new); ?> by <?php 
-		if (empty($url)) :
-		echo $author;
-	else:
-		echo "<a href='$url' rel='external'>$author_new</a>";
-	endif;
-	?>
-	</li>
-</ol>
-<form action="<?php echo get_settings('siteurl'); ?>/wp-comments-post.php" method="post" id="commentform">
-	<input type="hidden" name="author" id="author" class="textarea" value="<?php echo $author; ?>" />
-	<input type="hidden" name="comment_post_ID" value="<?php echo $comment_post_ID; ?>" />
-	<input type="hidden" name="redirect_to" value="<?php echo $redirect_to; ?>" />
-	<input type="hidden" name="email" id="email" value="<?php echo stripslashes($email); ?>" />
-	<input type="hidden" name="url" id="url" value="<?php echo $url; ?>" />
-	<input type="hidden" name="comment" id="comment" value="<?php echo htmlspecialchars(stripslashes($comment)); ?>" />
-	<input type="hidden" name="ticket" id="action"  value="<?php echo  get_ticket('comment_post') ?>" />
-	<input type="hidden" name="action" id="action"  value="confirm" />
-	<p>
-	  <input name="submit" type="submit" tabindex="5" value="Confirm" />
-	</p>
-</form>
-<h2><?php echo _LANG_WPCM_COM_LEAVE; ?></h2>
-<p><?php echo _LANG_WPCM_HTML_ALLOWED; ?><code><?php echo allowed_tags(); ?></code></p>
-<form action="<?php echo get_settings('siteurl'); ?>/wp-comments-post.php" method="post" id="commentform">
-	<p>
-	  <input type="text" name="author" id="author" class="textarea" value="<?php echo $author; ?>" size="28" tabindex="1" />
-	  <label for="author"><?php echo _LANG_WPCM_COM_NAME ?></label>
-	<input type="hidden" name="comment_post_ID" value="<?php echo $comment_post_ID; ?>" />
-	<input type="hidden" name="redirect_to" value="<?php echo $redirect_to; ?>" />
-	</p>
-
-	<p>
-	  <input type="text" name="email" id="email" value="<?php echo $email; ?>" size="28" tabindex="2" />
-	   <label for="email"><?php echo _LANG_WUS_AU_MAIL; ?></label>
-	</p>
-
-	<p>
-	  <input type="text" name="url" id="url" value="<?php echo $url; ?>" size="28" tabindex="3" />
-	   <label for="url"><?php echo _LANG_WUS_AU_URI; ?></label>
-	</p>
-
-	<p>
-	  <label for="comment"><?php echo _LANG_WPCM_COM_YOUR; ?></label>
-	<br />
-	  <textarea name="comment" id="comment" cols="70" rows="4" tabindex="4" ><?php echo stripslashes($comment); ?></textarea>
-	</p>
-
-	<p>
-	  <input name="submit" type="submit" tabindex="5" value="<?php echo _LANG_WPCM_COM_SAYIT; ?>" />
-	</p>
-</form>
-</div>
-<?php
+	$comment_preview = apply_filters('comment_text', $comment);
+	$author_preview = apply_filters('comment_author', $author);
+	$author_preview = (empty($url))? $author_new : "<a href='$url' rel='external'>$author_new</a>";
+	
+	$author_edit = sanitize_text($author);
+	$url_edit = sanitize_text($url,false,true);
+	$email_edit = sanitize_text($email);
+	$comment_edit = sanitize_text($comment);
+	$redirect_to_edit = sanitize_text($redirect_to,false,true);
+	include(get_custom_path('confirm-template.php'));
 	include(XOOPS_ROOT_PATH."/footer.php");
 	exit();
 } else {
 	if ((get_settings('use_comment_preview'))&&($action=='confirm')) {
-		verify_ticket('comment_post',$ticket,$location);
+		if ( ! $xoopsWPTicket->check() ) {
+			redirect_header($location,3,$xoopsWPTicket->getErrors());
+		}
 	}
-$now = current_time('mysql');
+	$now = current_time('mysql');
 
-$comment = balanceTags($comment, 1);
-$comment = convert_chars($comment);
-$comment = format_to_post($comment);
-$comment = apply_filters('post_comment_text', $comment);
+	$comment = balanceTags($comment, 1);
+	$comment = convert_chars($comment);
+	$comment = format_to_post($comment);
+	$comment = apply_filters('post_comment_text', $comment);
 
-$comment_author = $author;
-$comment_author_email = $email;
-$comment_author_url = $url;
+	$comment_author = $author;
+	$comment_author_email = $email;
+	$comment_author_url = $url;
 
-$author = addslashes($author);
-$email = addslashes($email);
-$url = addslashes($url);
-/* Flood-protection */
+	$author = addslashes($author);
+	$email = addslashes($email);
+	$url = addslashes($url);
+	/* Flood-protection */
 	$lasttime = $wpdb->get_var("SELECT comment_date FROM {$wpdb->comments[$wp_id]} 
-									WHERE comment_author_IP = '$user_ip' 
-									ORDER BY comment_date DESC LIMIT 1
-								");
-$ok = true;
-if (!empty($lasttime)) {
-	$time_lastcomment= mysql2date('U', $lasttime);
-	$time_newcomment= mysql2date('U', "$now");
-	if (($time_newcomment - $time_lastcomment) < 10)
-		$ok = false;
-}
-/* End flood-protection */
+								WHERE comment_author_IP = '$user_ip' 
+								ORDER BY comment_date DESC LIMIT 1
+							");
+	if (!empty($lasttime)) {
+		$time_lastcomment= mysql2date('U', $lasttime);
+		$time_newcomment= mysql2date('U', "$now");
+		if (($time_newcomment - $time_lastcomment) < 10) {
+		   	redirect_header($location ,5,_LANG_WPCP_SORRY_SECONDS);
+			exit;
+		}
+	}
+	/* End flood-protection */
 
-if ($ok) { // if there was no comment from this IP in the last 10 seconds
 	$comment_moderation = get_settings('comment_moderation');
 	$moderation_notify = get_settings('moderation_notify');
 	// o42: this place could be the hook for further comment spam checking
@@ -181,7 +129,7 @@ if ($ok) { // if there was no comment from this IP in the last 10 seconds
 	if (($moderation_notify) && (!$approved)) {
 	    wp_notify_moderator($comment_ID);
 	}
-	
+
 	if ((get_settings('comments_notify')) && ($approved)) {
 	    wp_notify_postauthor($comment_ID, 'comment');
 	}
@@ -206,9 +154,6 @@ if ($ok) { // if there was no comment from this IP in the last 10 seconds
 		header("Refresh: 0;url=$location");
 	} else {
 		header("Location: $location");
-	}
-} else {
-	   	redirect_header($location ,5,_LANG_WPCP_SORRY_SECONDS);
 	}
 }
 
