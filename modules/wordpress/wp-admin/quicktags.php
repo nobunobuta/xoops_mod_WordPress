f27eb1e7302d316e416621da3dd0905eb748aeef
nobunobuta <nobunobuta@nobunobuta.com>
2005-05-11 12:38:42 +0000

    FIX TYPO

--- modules/wordpress/wp-admin/quicktags.php
+++ modules/wordpress/wp-admin/quicktags.php
@@ -1,6 +1,5 @@
-<?php 
+<?php
     foreach($GLOBALS['wpsmiliestrans'][wp_id()] as $smiley => $img) {
-    { 
         print '<a href="javascript:bbinsert(document.post,\'\',\''.str_replace("'","\'",$smiley).'\')"><img src="' . $smilies_directory . '/'. $img . '" alt="' . $smiley . '" /></a> '; 
     } 
 echo "<br />"; 
