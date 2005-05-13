95e2605d54ba8626743e3066e85ae0b70065e63f
nobunobuta <nobunobuta@nobunobuta.com>
2005-05-13 14:22:47 +0000

    Enbal to modPukiWiki relocation

--- modules/wordpress/wp-content/plugins/pukiwiki.php
+++ modules/wordpress/wp-content/plugins/pukiwiki.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: PukiWiki
-Version: 0.3
+Version: 0.5
 Plugin URI: http://www.kowa.org/
 Description:PukiWiki Render
 Author: nobunobu
@@ -10,8 +10,6 @@ Author URI: http://www.kowa.org/
 if (!defined('WP_PLUGIN_PUKIWIKI')) {
 define('WP_PLUGIN_PUKIWIKI',1);
 function pukiwiki($text) {
-	include (dirname(__FILE__).'/modPukiWiki/PukiWiki.php') ;
-
 	$text = stripslashes($text);
 	//<!--more-->‚Ö‚Ì‘Î‰ž
 	$text = preg_replace("/\s*<a href='(.*?)#more-(.*?)'>(.*?)<\/a>/","\n\nRIGHT:[[\\3:\\1#more-\\2]]",$text);
@@ -29,31 +27,42 @@ function pukiwiki_com($text) {
 	return pukiwiki($text);
 }
 }
-remove_filter('the_content', 'wpautop');
-remove_filter('the_content', 'wptexturize');
-remove_filter('the_content', 'convert_bbcode');
-remove_filter('the_content', 'convert_gmcode');
-remove_filter('the_content', 'convert_smilies');
-remove_filter('the_content', 'convert_chars');
+if (class_exists('PukiiWikiRender')) {
+	if (file_exists(dirname(__FILE__).'/modPukiWiki/PukiWiki.php') {
+		include_once (dirname(__FILE__).'/modPukiWiki/PukiWiki.php') ;
+	} else if (file_exists(XOOPS_ROOT_PATH.'/common/modPukiWiki/PukiWiki.php') {
+		include_once(XOOPS_ROOT_PATH.'/common/modPukiWiki/PukiWiki.php');
+	} else if (file_exists(XOOPS_ROOT_PATH.'/class/modPukiWiki/PukiWiki.php') {
+		include_once(XOOPS_ROOT_PATH.'/class/modPukiWiki/PukiWiki.php');
+	}
+}
+if (class_exists('PukiiWikiRender')) {
+	remove_filter('the_content', 'wpautop');
+	remove_filter('the_content', 'wptexturize');
+	remove_filter('the_content', 'convert_bbcode');
+	remove_filter('the_content', 'convert_gmcode');
+	remove_filter('the_content', 'convert_smilies');
+	remove_filter('the_content', 'convert_chars');
 
-remove_filter('the_excerpt', 'wpautop');
-remove_filter('the_excerpt', 'wptexturize');
-remove_filter('the_excerpt', 'convert_bbcode');
-remove_filter('the_excerpt', 'convert_gmcode');
-remove_filter('the_excerpt', 'convert_smilies');
-remove_filter('the_excerpt', 'convert_chars');
+	remove_filter('the_excerpt', 'wpautop');
+	remove_filter('the_excerpt', 'wptexturize');
+	remove_filter('the_excerpt', 'convert_bbcode');
+	remove_filter('the_excerpt', 'convert_gmcode');
+	remove_filter('the_excerpt', 'convert_smilies');
+	remove_filter('the_excerpt', 'convert_chars');
 
-remove_filter('comment_text', 'wpautop',30);
-remove_filter('comment_text', 'wptexturize');
-remove_filter('comment_text', 'wp_filter_kses');
-remove_filter('comment_text', 'convert_bbcode');
-remove_filter('comment_text', 'convert_gmcode');
-remove_filter('comment_text', 'balanceTags');
-remove_filter('comment_text', 'convert_smilies',20);
-remove_filter('comment_text', 'convert_chars');
-remove_filter('comment_text', 'make_clickable');
+	remove_filter('comment_text', 'wpautop',30);
+	remove_filter('comment_text', 'wptexturize');
+	remove_filter('comment_text', 'wp_filter_kses');
+	remove_filter('comment_text', 'convert_bbcode');
+	remove_filter('comment_text', 'convert_gmcode');
+	remove_filter('comment_text', 'balanceTags');
+	remove_filter('comment_text', 'convert_smilies',20);
+	remove_filter('comment_text', 'convert_chars');
+	remove_filter('comment_text', 'make_clickable');
 
-add_filter('the_content', 'pukiwiki', 6);
-add_filter('the_excerpt', 'pukiwiki', 6);
-add_filter('comment_text', 'pukiwiki_com', 6);
+	add_filter('the_content', 'pukiwiki', 6);
+	add_filter('the_excerpt', 'pukiwiki', 6);
+	add_filter('comment_text', 'pukiwiki_com', 6);
+}
 ?>
