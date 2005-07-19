7ebda2fa3958ec348f7990c79e5a1d909b9d2698
nobunobuta <nobunobuta@nobunobuta.com>
2005-07-19 07:57:52 +0000

    投稿不具合対応(wp-comments.phpベースに全面書換）

--- modules/wordpress/wp-ktai-comments-post.php
+++ modules/wordpress/wp-ktai-comments-post.php
@@ -1,131 +1,154 @@
 <?php
-//   2004-07-05
-//   WordPress Ktai EditionópÉRÉÅÉìÉgÉ|ÉXÉgÉXÉNÉäÉvÉg
-//   $authorÇ∆$commentÇsjisÇ©ÇÁ$blog_charsetÇ…ïœä∑Ç∑ÇÈâ¸ë¢ÇÃÇ›ÅB
+require(dirname(__FILE__) . '/wp-config.php');
+if (get_xoops_option(wp_mod(), 'wp_use_xoops_comments')) return;
+init_param('POST', 'author', 'string','');
+init_param('POST', 'email', 'string','');
+init_param('POST', 'url', 'string','');
+init_param('POST', 'comment', 'html','');
+init_param('POST', 'comment_post_ID', 'integer','');
+init_param('POST', 'redirect_to', 'string','');
+init_param('POST', 'action', 'string','');
+init_param('POST', 'use_session', 'integer','');
 
-require( dirname(__FILE__) . '/wp-config.php' );
+$_author = get_param('author');
+$_email = get_param('email');
+$_url = get_param('url');
+$_comment = get_param('comment');
+$_comment_post_ID = get_param('comment_post_ID');
+$_redirect_to = get_param('redirect_to');
+$_action = get_param('action');
+$_use_session = get_param('use_session');
 
-if (!defined('XOOPS_URL')) {
-	$GLOBALS['blog_charset'] = get_settings('blog_charset');
-} else {
-	$tableposts = wp_table('posts');
-	$tablecomments = wp_table('comments');
+if (!is_email($_email)) {
+	$_email = '';
+}
+$_url_struct = parse_url($_url);
+if (!$_url_struct['path']) {
+	$_url = '';
+} elseif (!isset($_url_struct['scheme'])) {
+	$_url = 'http://'.$_url;
+} elseif (!preg_match('/^http[s]?$/',$_url_struct['scheme'])) {
+	$_url = '';
 }
 
-function add_magic_quotes($array) {
-	foreach ($array as $k => $v) {
-		if (is_array($v)) {
-			$array[$k] = add_magic_quotes($v);
-		} else {
-			$array[$k] = addslashes($v);
-		}
+$_location = (!$_redirect_to) ? $_SERVER['HTTP_REFERER'] : $_redirect_to;
+$_url_struct = parse_url($_location);
+if (isset($_url_struct['scheme']) && !preg_match('/^http[s]?$/',$_url_struct['scheme'])) {
+	$_location = wp_siteurl();
+}
+
+$_user_ip = $_SERVER['REMOTE_ADDR'];
+if ($_use_session) {
+	if ( ! $xoopsWPTicket->check() ) {
+		display_error($_location, 3, $xoopsWPTicket->getErrors());
 	}
-	return $array;
-} 
-
-if (!get_magic_quotes_gpc()) {
-	$_POST   = add_magic_quotes($_POST);
-	$_COOKIE = add_magic_quotes($_COOKIE);
+}
+$postHandler =& wp_handler('Post');
+$postObject =& $postHandler->get($_comment_post_ID);
+if (!$postObject) {
+   	display_error($_location ,5,_LANG_P_OOPS_IDCOM);
+}
+if ($postObject->getVar('comment_status') == 'closed') {
+   	display_error($_location ,5,_LANG_WPCP_SORRY_ITEM);
 }
 
-$author = trim(strip_tags($_POST['author']));
-$author = mb_convert_encoding($author, $GLOBALS['blog_charset'], "auto");
-
-$email = trim(strip_tags($_POST['email']));
-if (strlen($email) < 6)
-	$email = '';
-
-$url = trim(strip_tags($_POST['url']));
-$url = ((!stristr($url, '://')) && ($url != '')) ? 'http://'.$url : $url;
-if (strlen($url) < 7)
-	$url = '';
-
-$comment = trim($_POST['comment']);
-$comment_post_ID = intval($_POST['comment_post_ID']);
-$user_ip = $_SERVER['REMOTE_ADDR'];
-
-if ( 'closed' ==  $wpdb->get_var("SELECT comment_status FROM $tableposts WHERE ID = '$comment_post_ID'") )
-	die(_LANG_WPCP_SORRY_ITEM);
-
-if ( get_settings('require_name_email') && ('' == $email || '' == $author) )
-	die(_LANG_WPCP_ERR_FILL);
-
-if ( '' == $comment )
-	die(_LANG_WPCP_ERR_TYPE);
-
-
-$now = current_time('mysql');
-
-$comment = balanceTags($comment, 1);
-$comment = format_to_post($comment);
-$comment = apply_filters('post_comment_text', $comment);
-$comment = mb_convert_encoding($comment, $GLOBALS['blog_charset'], "auto");
-
-// Simple flood-protection
-$lasttime = $wpdb->get_var("SELECT comment_date FROM $tablecomments WHERE comment_author_IP = '$user_ip' ORDER BY comment_date DESC LIMIT 1");
-if (!empty($lasttime)) {
-	$time_lastcomment= mysql2date('U', $lasttime);
-	$time_newcomment= mysql2date('U', $now);
-	if (($time_newcomment - $time_lastcomment) < 10)
-		die(_LANG_WPCP_SORRY_SECONDS);
+if (get_settings('require_name_email') && ($_email == '' || $_author == '')) { //original fix by Dodo, and then Drinyth
+   	display_error($_location ,5,_LANG_WPCP_ERR_FILL);
+}
+if ($_comment == 'comment' || $_comment == '') {
+   	display_error($_location ,5,_LANG_WPCP_ERR_TYPE);
 }
 
-// If we've made it this far, let's post.
+$_now = current_time('mysql');
 
-if(!defined('XOOPS_URL')) {
-	$now_gmt = current_time('mysql', 1);
+$_comment = balanceTags($_comment, 1);
+$_comment = convert_chars($_comment);
+$_comment = apply_filters('post_comment_text', $_comment);
+$_comment_author = $_author;
+$_comment_author_email = $_email;
+$_comment_author_url = $_url;
 
-	if(check_comment($author, $email, $url, $comment, $user_ip)) {
-		$approved = 1;
-	} else {
-		$approved = 0;
-	}
-	$moderation_notify;
-	$wpdb->query("INSERT INTO $tablecomments 
-	(comment_post_ID, comment_author, comment_author_email, comment_author_url, comment_author_IP, comment_date, comment_date_gmt, comment_content, comment_approved) 
-	VALUES 
-	('$comment_post_ID', '$author', '$email', '$url', '$user_ip', '$now', '$now_gmt', '$comment', '$approved')
-	");
-} else {
-	$comment_moderation = get_settings('comment_moderation');
-	$moderation_notify = get_settings('moderation_notify');
-	if ('manual' == $comment_moderation) {
-		$approved = 0;
+$commentHandler =& wp_handler('Comment');
+
+/* Flood-protection */
+$_ok = true;
+$_criteria = new Criteria('comment_author_IP', $_user_ip);
+$_criteria->setSort('comment_date');
+$_criteria->setOrder('DESC');
+$_criteria->setLimit(1);
+$commentObjects = $commentHandler->getObjects($_criteria);
+if (count($commentObjects) > 0) {
+	$_lasttime = $commentObjects[0]->getVar('comment_date');
+	if ((mysql2date('U', "$_now") - mysql2date('U', $_lasttime)) < 10)
+		$_ok = false;
+}
+/* End flood-protection */
+
+if ($_ok) { // if there was no comment from this IP in the last 10 seconds
+	// o42: this place could be the hook for further comment spam checking
+	// $_approved should be set according the final approval status
+	// of the new comment
+	if (get_settings('comment_moderation') == 'manual') {
+		$_approved = 0;
 	} else { // none
-		$approved = 1;
+		$_approved = 1;
 	}
-	$wpdb->query("INSERT INTO $tablecomments 
-	(comment_post_ID, comment_author, comment_author_email, comment_author_url, comment_author_IP, comment_date, comment_content, comment_approved) 
-	VALUES 
-	('$comment_post_ID', '$author', '$email', '$url', '$user_ip', '$now', '$comment', '$approved')
-	");
-}
+	$commentObject =& $commentHandler->create();
+	$commentObject->setVar('comment_post_ID',$_comment_post_ID);
+	$commentObject->setVar('comment_author',$_author);
+	$commentObject->setVar('comment_author_email',$_email);
+	$commentObject->setVar('comment_author_url',$_url);
+	$commentObject->setVar('comment_author_IP',$_user_ip);
+	$commentObject->setVar('comment_date',$_now);
+	$commentObject->setVar('comment_content',$_comment);
+	$commentObject->setVar('comment_approved',$_approved);
+	if(!$commentHandler->insert($commentObject)) {
+		display_error($_location, 3, $commentHandler->getErrors());
+	}
+	$_comment_ID = $commentObject->getVar('comment_ID');
+	do_action('comment_post', $_comment_ID);
+	if ((get_settings('moderation_notify')) && (!$_approved)) {
+	    wp_notify_moderator($_comment_ID);
+	}
+	if ((get_settings('comments_notify')) && ($_approved)) {
+	    wp_notify_postauthor($_comment_ID, 'comment');
+	}
+	if ($_email == '')
+		$_email = ' '; // this to make sure a cookie is set for 'no email'
+	if ($_url == '')
+		$_url = ' '; // this to make sure a cookie is set for 'no url'
 
-$comment_ID = $wpdb->get_var('SELECT last_insert_id()');
-
-if (($moderation_notify) && (!$approved)) {
-	wp_notify_moderator($comment_ID);
-}
-
-if ((get_settings('comments_notify')) && ($approved)) {
-	wp_notify_postauthor($comment_ID, 'comment');
-}
-
-do_action('comment_post', $comment_ID);
-
-setcookie('comment_author_' . $cookiehash, $author, time() + 30000000, COOKIEPATH);
-setcookie('comment_author_email_' . $cookiehash, $email, time() + 30000000, COOKIEPATH);
-setcookie('comment_author_url_' . $cookiehash, $url, time() + 30000000, COOKIEPATH);
-
-header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
-header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
-header('Cache-Control: no-cache, must-revalidate');
-header('Pragma: no-cache');
-$location = (empty($_POST['redirect_to'])) ? $_SERVER["HTTP_REFERER"] : $_POST['redirect_to'];
-if ($is_IIS) {
-	header("Refresh: 0;url=$location");
+	setcookie('comment_author_'.$GLOBALS['cookiehash'], $_author, time()+30000000);
+	setcookie('comment_author_email_'.$GLOBALS['cookiehash'], $_email, time()+30000000);
+	setcookie('comment_author_url_'.$GLOBALS['cookiehash'], $_url, time()+30000000);
+	header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+	header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
+	header('Cache-Control: no-cache, must-revalidate');
+	header('Pragma: no-cache');
+	if ($GLOBALS['is_IIS']) {
+		header('Refresh: 0;url='.$_location);
+	} else {
+		header('Location: '.$_location);
+	}
 } else {
-	header("Location: $location");
+	display_error($_location ,5,_LANG_WPCP_SORRY_SECONDS);
 }
-
+function display_error($url, $dum, $mess) {
+	$blogname = get_bloginfo('name'); //•÷•Ì•∞§Œ•ø•§•»•Î
+	$echostring  = '<html>';
+	$echostring .= '<!--»˛∆˝-->';
+	$echostring .= '<head>';
+	$echostring .= '<title>'.$blogname.' Ktai edition</title>';
+	$echostring .= '<meta http-equiv="Content-Type" content="text/html; charset=Shift_JIS" />';
+	$echostring .= '</head>';
+	$echostring .= '<body>';
+	$echostring .= '<center>'.$blogname.'<br />Ktai edition</center><hr />';
+	$echostring .= 'Error : '.$mess.'<hr>';
+	$echostring .= '<a href="'.$url.'">Ã·§Î</a>';
+	$echostring .= '</body>';
+	$echostring .= '</html>';
+	header("Content-Type: text/html; charset=Shift_JIS");
+	echo mb_convert_encoding($echostring, "sjis", "auto");
+	exit();
+}
 ?>
