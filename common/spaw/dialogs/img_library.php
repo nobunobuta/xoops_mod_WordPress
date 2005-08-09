49cab5523318e31aadba3d3aed2003fba41c22f8
nobunobuta <nobunobuta@nobunobuta.com>
2005-08-09 03:53:35 +0000

    Simplified URLsとの共存時のエラー発生への対応

--- common/spaw/dialogs/img_library.php
+++ common/spaw/dialogs/img_library.php
@@ -102,9 +102,9 @@ if ($spaw_img_delete_allowed && isset($_POST['lib_action'])
   <meta http-equiv="Content-Type" content="text/html; charset=<?php echo $l->getCharset()?>">
   <link rel="stylesheet" type="text/css" href="<?php echo $theme_path.'css/'?>dialog.css">
   <?php if (SPAW_Util::getBrowser() == 'Gecko') { ?>
-  <script language="javascript" src="utils.gecko.js"></script>
+  <script language="javascript" src="<?php echo $spaw_dir ?>/dialogs/utils.gecko.js"></script>
   <?php }else{ ?>
-  <script language="javascript" src="utils.js"></script>
+  <script language="javascript" src="<?php echo $spaw_dir ?>/dialogs/utils.js"></script>
   <?php } ?>
   
   <script language="javascript">
