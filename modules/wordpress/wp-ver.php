1f75db24f027e05b91a18f4190e640c26d6a8106
nobunobuta <nobunobuta@nobunobuta.com>
2005-09-20 12:34:28 +0000

    WordPress 0.6.0 Beta

--- modules/wordpress/wp-ver.php
+++ modules/wordpress/wp-ver.php
@@ -1,8 +1,8 @@
 <?php
 	$_wp_version_major = '0';
-	$_wp_version_minor = '5';
+	$_wp_version_minor = '6';
 	$_wp_version_mod   = '0';
-	$_wp_version_fix   = '-RC6';
+	$_wp_version_fix   = '-Beta';
 	$GLOBALS['wp_version_str']   = $_wp_version_major.'.'.$_wp_version_minor.'.'.$_wp_version_mod.$_wp_version_fix;
 	$GLOBALS['wp_version_xoops'] = $_wp_version_major.'.'.$_wp_version_minor.$_wp_version_mod;
 ?>
