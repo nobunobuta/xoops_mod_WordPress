7fdd8d77f79d9c96bee0f750ac973f93b37a63c4
nobunobuta <nobunobuta@nobunobuta.com>
2006-06-04 08:15:52 +0000

    GIJOE氏のgticket2に準拠した再投稿機能の実装

---
+++ modules/wordpress/language/japanese/wp_ticket.msg.php
@@ -0,0 +1,13 @@
+<?php
+		// default messages
+		if( empty( $this->messages ) ) $this->messages = array(
+			'err_general' => 'チケットのエラーです。' ,
+			'err_nostubs' => 'セッションにスタブが見つかりません。' ,
+			'err_noticket' => 'リクエストにチケットが見つかりません。' ,
+			'err_nopair' => '有効なチケットとスタブの組合せが見つかりませんでした。' ,
+			'err_timeout' => 'タイムアウトしました。' ,
+			'err_areaorref' => '無効なエリアもしくはリファラです。' ,
+			'fmt_prompt4repost' => '<h2>エラーが発生しました</h2><span style="background-color:red;font-weight:bold;color:white;">%s</span><br />確認して、再投稿したい場合には再投稿ボタンを押して下さい。' ,
+			'btn_repost' => '再投稿' ,
+		) ;
+?>
--- modules/wordpress/wp-includes/wp-tickets.php
+++ modules/wordpress/wp-includes/wp-tickets.php
@@ -11,7 +11,29 @@ class _XoopsNTicket {
 	var $_session_param_name = 'XOOPS_N_STUBS';
 	var $_max_stub_size = 10;
 	var $_default_timeout = 1800;
+	var $messages;
 	
+	function _XoopsNTicket()
+	{
+		// language file
+		if( defined( 'XOOPS_ROOT_PATH' ) && ! empty( $GLOBALS['xoopsConfig']['language'] ) && ! strstr( $GLOBALS['xoopsConfig']['language'] , '/' ) ) {
+			if( file_exists( dirname( dirname( __FILE__ ) ) . '/language/' . $GLOBALS['xoopsConfig']['language'] . '/wp_ticket.msg.php' ) ) {
+				include dirname( dirname( __FILE__ ) ) . '/language/' . $GLOBALS['xoopsConfig']['language'] . '/wp_ticket.msg.php' ;
+			}
+		}
+		// default messages
+		if( empty( $this->messages ) ) $this->messages = array(
+			'err_general' => 'Ticket Error' ,
+			'err_nostubs' => 'No stubs found' ,
+			'err_noticket' => 'No ticket found' ,
+			'err_nopair' => 'No valid ticket-stub pair found' ,
+			'err_timeout' => 'Time out' ,
+			'err_areaorref' => 'Invalid area or referer' ,
+			'fmt_prompt4repost' => '<h2>Error(s) found</h2><span style="background-color:red;font-weight:bold;color:white;">%s</span><br />Confirm it.<br />And do you want to post again?' ,
+			'btn_repost' => 'repost' ,
+		) ;
+	}
+
 	// render form as plain html
 	function getTicketHtml( $salt = '' , $timeout = -1 , $area = '' )
 	{
@@ -22,6 +44,12 @@ class _XoopsNTicket {
 	function getTicketXoopsForm( $salt = '' , $timeout = -1 , $area = '' )
 	{
 		return new XoopsFormHidden( $this->_post_param_name , $this->issue( $salt , $timeout , $area) ) ;
+	}
+
+	// add a ticket as Hidden Element into XoopsForm
+	function addTicketXoopsFormElement( &$form , $salt = '' , $timeout = 1800 , $area = '' )
+	{
+		$form->addElement( new XoopsFormHidden( $this->_post_param_name  , $this->issue( $salt , $timeout , $area ) ) ) ;
 	}
 
 	// returns an array for xoops_confirm() ;
@@ -39,6 +67,8 @@ class _XoopsNTicket {
 	// issue a ticket
 	function issue( $salt = '' , $timeout = -1 , $area = '' )
 	{
+		global $xoopsModule ;
+
 		if ($timeout==-1) $timeout = $this->_default_timeout;
 		$param_name = $this->_session_param_name;
 
@@ -78,16 +108,18 @@ class _XoopsNTicket {
 	}
 
 	// check a ticket
-	function check($reqtype='BOTH', $area = '' )
+	function check($reqtype='BOTH', $area = '', $allow_repost = true  )
 	{
+		global $xoopsModule ;
+
 		$this->_errors = array() ;
+
 		$param_name = $this->_session_param_name;
 
 		// CHECK: stubs are not stored in session
 		if( empty( $_SESSION[$param_name] ) || !is_array($_SESSION[$param_name])) {
-			$this->clear() ;
-			$this->_errors[] = "Invalid Session[$param_name]" ;
-			return false ;
+			$this->_errors[] = $this->messages['err_nostubs'] ;
+			$_SESSION[$param_name] = array() ;
 		}
 
 		// get key&val of the ticket from a user's query
@@ -105,20 +137,20 @@ class _XoopsNTicket {
 
 		// CHECK: no tickets found
 		if( empty( $ticket ) ) {
-			$this->clear() ;
-			$this->_errors[] = 'Irregular post found' ;
-			return false ;
+			$this->_errors[] = $this->messages['err_noticket'] ;
 		}
 
 		// gargage collection & find a right stub
-		$stubs_tmp = array() ;
-		foreach( $_SESSION[$param_name] as $stub ) {
+		$stubs_tmp = $_SESSION[$param_name] ;
+		$_SESSION[$param_name] = array() ;
+		foreach( $stubs_tmp as $stub ) {
+			// default lifetime 30min
 			if( $stub['expire'] >= time() ) {
 				if (md5( $stub['token'].XOOPS_DB_PREFIX ) === $ticket) {
 					$found_stub = $stub ;
 				} else {
 					// store the other valid stubs into session
-					$stubs_tmp[] = $stub ;
+					$_SESSION[$param_name] = $stub ;
 				}
 			} else {
 				if(md5( $stub['token'] . XOOPS_DB_PREFIX ) === $ticket) {
@@ -127,35 +159,115 @@ class _XoopsNTicket {
 				}
 			}
 		}
-		$_SESSION[$param_name] = $stubs_tmp;
 
 		// CHECK: the right stub found or not
 		if( empty( $found_stub ) ) {
-//			$this->clear() ;
-			if( empty( $timeout_flag ) ) $this->_errors[] = 'Invalid Session' ;
-			else $this->_errors[] = 'Time out' ;
-			return false ;
+			if( empty( $timeout_flag ) ) $this->_errors[] = $this->messages['err_nopair'] ;
+			else $this->_errors[] = $this->messages['err_timeout'] ;
+		} else {
+
+		    // set area if necessary
+		    // area as module's dirname
+		    if( ! $area && is_object( @$xoopsModule ) ) {
+			    $area = $xoopsModule->getVar('dirname') ;
+		    }
+    
+		    // check area or referer
+		    if( @$found_stub['area'] == $area ) $area_check = true ;
+		    if( ! empty( $found_stub['referer'] ) && strstr( @$_SERVER['HTTP_REFERER'] , $found_stub['referer'] ) ) $referer_check = true ;
+    
+		    if( empty( $area_check ) && empty( $referer_check ) ) { // loose
+			    $this->_errors[] = $this->messages['err_areaorref'] ;
+		    }
 		}
 
-		// set area if necessary
-		// area as module's dirname
-		if( ! $area && is_object( @$xoopsModule ) ) {
-			$area = $xoopsModule->getVar('dirname') ;
-		}
-
-		// check area or referer
-		if( @$found_stub['area'] == $area ) $area_check = true ;
-		if( ! empty( $found_stub['referer'] ) && strstr( @$_SERVER['HTTP_REFERER'] , $found_stub['referer'] ) ) $referer_check = true ;
-
-		// if( empty( $area_check ) || empty( $referer_check ) ) { // restrict
-		if( empty( $area_check ) && empty( $referer_check ) ) { // loose
+		if( ! empty( $this->_errors ) ) {
+			if( $allow_repost ) {
+				// repost form
+				$this->draw_repost_form( $area ) ;
+				exit ;
+			} else {
+				// failed
 				$this->clear() ;
-			$this->_errors[] = 'Invalid area or referer' ;
-			return false ;
+				return false ;
+			}
+		} else {
+			// all green
+			return true;
 		}
+	}
 
-		// all green
-		return true;
+	// draw form for repost
+	function draw_repost_form( $area = '' )
+	{
+		// Notify which file is broken
+		if( headers_sent() ) {
+			restore_error_handler() ;
+			set_error_handler( '_XoopsNTicket_ErrorHandler4FindOutput' ) ;
+			header( 'Dummy: for warning' ) ;
+			restore_error_handler() ;
+			exit ;
+		}
+	
+		error_reporting( 0 ) ;
+		while( ob_get_level() ) ob_end_clean() ;
+	    global $xoopsTpl, $xoopsConfig, $xoopsOption, $xoopsLogger;
+	    if (file_exists(XOOPS_ROOT_PATH.'/themes/default/theme.html')) {
+	        $xoopsConfig['theme_set'] = 'default';
+	    }
+        unset($xoopsOption['template_main']);
+        $xoopsTpl =& new XoopsTpl();
+    	$xoopsOption['theme_use_smarty'] = 1;
+        $xoopsConfig['debug_mode'] = 0;
+		ob_start();
+    	$xoopsTpl->assign(array('xoops_theme' => $xoopsConfig['theme_set'], 'xoops_imageurl' => XOOPS_THEME_URL.'/'.$xoopsConfig['theme_set'].'/', 'xoops_themecss'=> xoops_getcss($xoopsConfig['theme_set']), 'xoops_sitename' => htmlspecialchars($xoopsConfig['sitename'], ENT_QUOTES), 'xoops_slogan' => htmlspecialchars($xoopsConfig['slogan'], ENT_QUOTES)));
+		$table = '<table class="outer">' ;
+		$form = '<form action="?'.htmlspecialchars(@$_SERVER['QUERY_STRING'],ENT_QUOTES).'" method="post" >' ;
+		foreach( $_POST as $key => $val ) {
+			if( $key == $this->_post_param_name ) continue ;
+			if( get_magic_quotes_gpc() ) {
+				$key = stripslashes( $key ) ;
+			}
+			if( is_array( $val ) ) {
+				list( $tmp_table , $tmp_form ) = $this->extract_post_recursive( htmlspecialchars($key,ENT_QUOTES) , $val ) ;
+				$table .= $tmp_table ;
+				$form .= $tmp_form ;
+			} else {
+				if( get_magic_quotes_gpc() ) {
+					$val = stripslashes( $val ) ;
+				}
+				$table .= '<tr><th><code>'.htmlspecialchars($key,ENT_QUOTES).'</code></th><td class="odd"><pre style="margin:0;padding:0;overflow:auto;"><code>'.htmlspecialchars($val,ENT_QUOTES).'</code></pre></td></tr>'."\n" ;
+				$form .= '<input type="hidden" name="'.htmlspecialchars($key,ENT_QUOTES).'" value="'.htmlspecialchars($val,ENT_QUOTES).'" />'."\n" ;
+			}
+		}
+		$table .= '</table>' ;
+		$form .= $this->getTicketHtml(__LINE__,300,$area).'<input type="submit" value="'.$this->messages['btn_repost'].'" /></form>' ;
+
+//		echo '<html><head><title>'.$this->messages['err_general'].'</title><style>table,td,th {border:solid black 1px; border-collapse:collapse;}</style></head><body>' . sprintf( $this->messages['fmt_prompt4repost'] , $this->getErrors() ) . $table . $form . '</body></html>' ;
+		echo sprintf( $this->messages['fmt_prompt4repost'] , $this->getErrors() ) . $table . $form ;
+        include XOOPS_ROOT_PATH.'/footer.php';
+	}
+
+	function extract_post_recursive( $key_name , $tmp_array ) {
+		$table = '' ;
+		$form = '' ;
+		foreach( $tmp_array as $key => $val ) {
+			if( get_magic_quotes_gpc() ) {
+				$key = stripslashes( $key ) ;
+			}
+			if( is_array( $val ) ) {
+				list( $tmp_table , $tmp_form ) = $this->extract_post_recursive( $key_name.'['.htmlspecialchars($key,ENT_QUOTES).']' , $val ) ;
+				$table .= $tmp_table ;
+				$form .= $tmp_form ;
+			} else {
+				if( get_magic_quotes_gpc() ) {
+					$val = stripslashes( $val ) ;
+				}
+				$table .= '<tr><th><code>'.$key_name.'['.htmlspecialchars($key,ENT_QUOTES).']</code></th><td class="odd"><pre style="margin:0;padding:0;overflow:auto;"><code>'.htmlspecialchars($val,ENT_QUOTES).'</code></pre></td></tr>'."\n" ;
+				$form .= '<input type="hidden" name="'.$key_name.'['.htmlspecialchars($key,ENT_QUOTES).']" value="'.htmlspecialchars($val,ENT_QUOTES).'" />'."\n" ;
+			}
+		}
+		return array( $table , $form ) ;
 	}
 
 
@@ -202,6 +314,7 @@ class XoopsWPTicket extends _XoopsNTicket {
 	// intialize parameter names
 	function XoopsWPTicket()
 	{
+	    parent::_XoopsNTicket();
 		$this->_post_param_name = 'XOOPS_WP_TICKET';
 		$this->_session_param_name = 'XOOPS_WP_STUBS';
 		$this->_max_stub_size = 20;
@@ -211,5 +324,17 @@ class XoopsWPTicket extends _XoopsNTicket {
 // create a instance in global scope
 $GLOBALS['xoopsWPTicket'] = new XoopsWPTicket() ;
 
+}
+
+if( ! function_exists( '_XoopsNTicket_ErrorHandler4FindOutput' ) ) {
+function _XoopsNTicket_ErrorHandler4FindOutput($errNo, $errStr, $errFile, $errLine)
+{
+	if( preg_match( '?'.preg_quote(XOOPS_ROOT_PATH).'([^:]+)\:(\d+)?' , $errStr , $regs ) ) {
+		echo "Irregular output! check the file ".htmlspecialchars($regs[1])." line ".htmlspecialchars($regs[2]) ;
+	} else {
+		echo "Irregular output! check language files etc." ;
+	}
+	return ;
+}
 }
 ?>
